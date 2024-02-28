282bfd906d659de013d7148faac93a4bcbf57bb4
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-02-28 17:42:10 -0300

    Add others arguments for program.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser(
     ,description="Program for complete scan website.")
 parser.add_argument('-u',"--url",type=str, nargs="?", action='store',help="Url for scan.") #   URL      |   argument     |   store
 parser.add_argument('-w',"--wordlist",type=str, nargs="?", action='store',help="Wordlist for used scan.") #   URL      |   argument     |   store
+parser.add_argument('-c',"--cookies",type=str, nargs="?", action='store',help="Cookies for used scan.") #   URL      |   argument     |   store
+parser.add_argument('-h',"--headers",type=str, nargs="?", action='store',help="Headers for used scan.") #   URL      |   argument     |   store
 parser.add_argument('-v','--verbose',action='store_true',help='Used for verbose scan.')    #   VERBOSE  |   argument     |   store_true
 
 args = parser.parse_args()
